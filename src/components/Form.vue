001703f0bb50892e64b9b9f965c2338963ecb212
Adam4ever <adam4ever@gmail.com>
2023-10-17 07:11:42 -0300

    Init Commit

---
+++ src/components/Form.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="form">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" id="name" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label for="init">Initiative</label>
+      <input type="number" id="init" class="form-control" />
+    </div>
+    <button type="submit" class="btn">Add</button>
+  </form>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Welcome</h1>
+    <div class="card">
+      <Form />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Form from '@/components/Form.vue';
+</script>
